e6259225352bce9f598b0efa15cb1558701ff049
Joao Vitor Cordeiro <joao.csilva@spread.com.br>
2023-01-04 21:30:08 -0300

    Iniciando Projeto

---
+++ android/app/src/main/kotlin/com/example/pokedex_incie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pokedex_incie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ run_app.sh
@@ -0,0 +1,6 @@
+echo "Clean all packages"
+  flutter clean
+echo "Get in all packages"
+  flutter pub get
+echo "Run App"
+  flutter run -t lib/src/main.dart
